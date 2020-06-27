9f80ee9267d98568f347b718bcc4d3f335a62591
bkthomps <bkthomps@edu.uwaterloo.ca>
2020-06-27 19:32:15 -0400

    Clean up the code

--- main.cpp
+++ main.cpp
@@ -1,3 +1,5 @@
+#pragma ide diagnostic ignored "hicpp-signed-bitwise"
+
 #include <iostream>
 #include <vector>
 #include <thread>
@@ -5,56 +7,69 @@
 
 extern "C" int SHA3_224(unsigned char *, const unsigned char *, size_t);
 
-std::string get_input(const int thread_number) {
-    auto h_m = std::string("<put h(amt0) for nonce 1, or h(m1) for nonce 2; pad with zeroes>");
-    auto m = std::string("<put m1 for nonce 1, or m2 for nonce 2; pad with zeroes>");
-    if (h_m.size() != 224 || m.size() != 4104) {
-        std::cerr << "Bad size, h_m = " << h_m.size() << ", m = " << m.size() << std::endl;
-        exit(-1);
-    }
+#define HM_BITS 224
+#define M_BITS 4104
+#define NONCE_BITS 128
+#define NON_NONCE_BITS (HM_BITS + M_BITS)
+#define PRE_IMG_BITS (NON_NONCE_BITS + NONCE_BITS)
+#define NON_NONCE_BYTES (NON_NONCE_BITS / 8)
+#define PRE_IMG_BYTES (PRE_IMG_BITS / 8)
+#define HASH_BYTES 28
+
+std::string get_zeros(const int thread_number) {
     auto zeros = std::string("");
-    for (int i = 0; i < 128; i++) {
+    for (int i = 0; i < NONCE_BITS; i++) {
         zeros += "0";
     }
-    auto input = h_m + m + zeros;
     switch (thread_number) {
         case 0:
             break;
         case 1:
-            input[input.size() - 128] = '1';
+            zeros[0] = '1';
             break;
         case 2:
-            input[input.size() - 127] = '1';
+            zeros[1] = '1';
             break;
         case 3:
-            input[input.size() - 128] = '1';
-            input[input.size() - 127] = '1';
+            zeros[0] = '1';
+            zeros[1] = '1';
             break;
         case 4:
-            input[input.size() - 126] = '1';
+            zeros[2] = '1';
             break;
         case 5:
-            input[input.size() - 128] = '1';
-            input[input.size() - 126] = '1';
+            zeros[0] = '1';
+            zeros[2] = '1';
             break;
         case 6:
-            input[input.size() - 127] = '1';
-            input[input.size() - 126] = '1';
+            zeros[1] = '1';
+            zeros[2] = '1';
             break;
         case 7:
-            input[input.size() - 128] = '1';
-            input[input.size() - 127] = '1';
-            input[input.size() - 126] = '1';
+            zeros[0] = '1';
+            zeros[1] = '1';
+            zeros[2] = '1';
             break;
         default:
             std::cerr << "Maximum of 8 threads" << std::endl;
             exit(-1);
     }
-    return input;
+    return zeros;
+}
+
+std::string get_input(const int thread_number) {
+    auto h_m = std::string("<put h(amt0) for nonce 1, or h(m1) for nonce 2; pad with zeroes>");
+    auto m = std::string("<put m1 for nonce 1, or m2 for nonce 2; pad with zeroes>");
+    if (h_m.size() != HM_BITS || m.size() != M_BITS) {
+        std::cerr << "Bad size, h_m = " << h_m.size() << ", m = " << m.size() << std::endl;
+        exit(-1);
+    }
+    auto zeros = get_zeros(thread_number);
+    return h_m + m + zeros;
 }
 
 bool increase(unsigned char *input_arr) {
-    for (unsigned long i = 224 + 4104 + 128 - 1; i >= 224 + 4104; i--) {
+    for (unsigned long i = PRE_IMG_BITS - 1; i >= NON_NONCE_BITS; i--) {
         unsigned long byte = i / 8;
         unsigned long offset = 7 - i % 8;
         unsigned int bit = (input_arr[byte] & (1U << offset)) >> offset;
@@ -68,26 +83,25 @@ bool increase(unsigned char *input_arr) {
 
 void thread(const int thread_number) {
     auto input_str = get_input(thread_number);
-    int bytes = (224 + 4104 + 128) / 8;
-    unsigned char input_arr[bytes + 1];
-    input_arr[bytes] = '\0';
-    for (int i = 0; i < bytes; i++) {
+    unsigned char input_arr[PRE_IMG_BYTES + 1];
+    input_arr[PRE_IMG_BYTES] = '\0';
+    for (int i = 0; i < PRE_IMG_BYTES; i++) {
         unsigned char value = 0;
         for (int j = 0; j < 8; j++) {
             value |= (input_str.at(8 * i + j) - '0') << (7 - j);
         }
         input_arr[i] = value;
     }
-    unsigned char pre_image_buffer[64];
-    memset(pre_image_buffer, 0, 64);
+    unsigned char pre_image_buffer[HASH_BYTES + 1];
+    memset(pre_image_buffer, 0, HASH_BYTES + 1);
     do {
-        SHA3_224(pre_image_buffer, input_arr, bytes);
+        SHA3_224(pre_image_buffer, input_arr, PRE_IMG_BYTES);
         if (pre_image_buffer[0] == '\0' && pre_image_buffer[1] == '\0'
             && pre_image_buffer[2] == '\0' && pre_image_buffer[3] == '\0') {
             std::string nonce("Nonce (thread ");
             nonce += std::to_string(thread_number);
             nonce += "): ";
-            for (int i = (224 + 4104) / 8; i < (224 + 4104 + 128) / 8; i++) {
+            for (int i = NON_NONCE_BYTES; i < PRE_IMG_BYTES; i++) {
                 nonce += std::to_string((input_arr[i] & 0b10000000) >> 7);
                 nonce += std::to_string((input_arr[i] & 0b01000000) >> 6);
                 nonce += std::to_string((input_arr[i] & 0b00100000) >> 5);
@@ -101,7 +115,7 @@ void thread(const int thread_number) {
             std::string pre_image("Pre-Image (thread ");
             pre_image += std::to_string(thread_number);
             pre_image += "): ";
-            for (int i = 0; i < 28; i++) {
+            for (int i = 0; i < HASH_BYTES; i++) {
                 pre_image += std::to_string((pre_image_buffer[i] & 0b10000000) >> 7);
                 pre_image += std::to_string((pre_image_buffer[i] & 0b01000000) >> 6);
                 pre_image += std::to_string((pre_image_buffer[i] & 0b00100000) >> 5);
